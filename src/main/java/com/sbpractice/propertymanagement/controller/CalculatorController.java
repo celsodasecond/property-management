bde54eafe2a388d3fd601ae91cc10f0b6c30fb27
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2023-08-20 00:56:51 +0800

    add: Alternate way for mapping values of url

--- src/main/java/com/sbpractice/propertymanagement/controller/CalculatorController.java
+++ src/main/java/com/sbpractice/propertymanagement/controller/CalculatorController.java
@@ -1,9 +1,6 @@
 package com.sbpractice.propertymanagement.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/calculator")
@@ -12,5 +9,16 @@ public class CalculatorController {
     @GetMapping("/add")
     public Double add(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2) {
         return num1 + num2;
+    }
+
+    // Another way - Map the values of url to java variables by Path Variable Method
+    @GetMapping("/subtract/{num1}/{num2}")
+    public Double subtract(@PathVariable("num1") Double num1,@PathVariable("num2") Double num2) {
+        Double result = null;
+        result = (num1 > num2)
+                ? num1 - num2
+                : num2 - num1;
+
+        return result;
     }
 }
